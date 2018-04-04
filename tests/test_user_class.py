8020cd094a41e994e3d6ac34a694409a17d18641
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-04-04 09:11:36 +0200

    Very simple test for User class

---
+++ tests/test_user_class.py
@@ -0,0 +1,16 @@
+"""Unit tests for the authorization module."""
+
+import unittest
+from f8a_auth import User
+
+
+def test_user_class():
+    """Test the basic behaviour of User class."""
+    email = "tester@foo.bar.baz"
+    user = User(email)
+    # just dummy check ATM as the class is pretty simple
+    assert email == user.email
+
+
+if __name__ == '__main__':
+    test_user_class()
