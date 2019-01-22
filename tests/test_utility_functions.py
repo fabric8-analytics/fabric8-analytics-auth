c30fd938a06135c37059296c7a855b9345ccdd63
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-01-22 09:14:13 +0100

    Test for the function is_authentication_disabled

--- tests/test_utility_functions.py
+++ tests/test_utility_functions.py
@@ -5,5 +5,38 @@ import fabric8a_auth.auth import is_authentication_disabled
 import os
 
 
+def test_is_authentication_disabled_negative_test():
+    """Test the function is_authentication_disabled()."""
+    os.environ.set('DISABLE_AUTHENTICATION', '')
+    assert not is_authentication_disabled()
+
+    os.environ.set('DISABLE_AUTHENTICATION', '0')
+    assert not is_authentication_disabled()
+
+    os.environ.set('DISABLE_AUTHENTICATION', 'false')
+    assert not is_authentication_disabled()
+
+    os.environ.set('DISABLE_AUTHENTICATION', 'False')
+    assert not is_authentication_disabled()
+
+    os.environ.set('DISABLE_AUTHENTICATION', 'FALSE')
+    assert not is_authentication_disabled()
+
+
+def test_is_authentication_disabled_positive_test():
+    """Test the function is_authentication_disabled()."""
+
+    os.environ['DISABLE_AUTHENTICATION'] = '1'
+    assert is_authentication_disabled()
+
+    os.environ['DISABLE_AUTHENTICATION'] = 'True'
+    assert is_authentication_disabled()
+
+    os.environ['DISABLE_AUTHENTICATION'] = 'true'
+    assert is_authentication_disabled()
+
+    os.environ['DISABLE_AUTHENTICATION'] = 'TRUE'
+    assert is_authentication_disabled()
 if __name__ == '__main__':
-    pass
+    test_is_authentication_disabled_negative_test()
+    test_is_authentication_disabled_positive_test()
