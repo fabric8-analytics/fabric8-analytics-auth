e10d7cda167d78c277f318e47c867848c2b36317
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-08-15 15:19:06 +0200

    X

--- cico_run_pydoc.sh
+++ cico_run_pydoc.sh
@@ -3,15 +3,16 @@
 set -ex
 
 prep() {
-    yum –disableplugin=fastestmirror -y update
-    yum –disableplugin=fastestmirror -y install epel-release
-    yum –disableplugin=fastestmirror -y install python36 python36-virtualenv which
+    yum -y update
+    yum -y install epel-release
+    yum -y install python36 python36-virtualenv which
 }
 
 check_python_version() {
     python3 tools/check_python_version.py 3 6
 }
 
+cat /etc/yum/pluginconf.d/fastestmirror.conf
 prep
 check_python_version
 ./check-docstyle.sh
